f41c81b6a363b8b0c4f5aafe21a831b2378e572c
KazumaFujino <c1000702@planet.kanazawa-it.ac.jp>
2023-02-22 19:12:51 +0900

    ✨feat:射出インタフェース

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -4,6 +4,7 @@
 #include "mcl_2d/mcl_2d_node.hpp"
 #include "controller_interface/controller_interface_node.hpp"
 #include "spline_pid/spline_pid_node.hpp"
+#include "injection_interface/injection_interface_node.hpp"
 #include <iostream>
 
 int main(int argc, char * argv[]){
@@ -14,15 +15,17 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
-    // auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
+    auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     auto mcl_2d_node = std::make_shared<mcl_2d::Mcl2D>(nodes_option);
     auto controller_node = std::make_shared<controller_interface::ControllerInterface>(nodes_option);
     auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
+    auto injection_interface_node0 = std::make_shared<injection_interface::InjectionInterface>(nodes_option, 0);
 
     // exec.add_node(socketcan_node);
     exec.add_node(mcl_2d_node);
     exec.add_node(controller_node);
     exec.add_node(spline_pid_node);
+    exec.add_node(injection_interface_node0);
 
     exec.spin();
     rclcpp::shutdown();

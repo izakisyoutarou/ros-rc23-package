e6478a577ca318ccdf6bee8172ccf16393d6b7b7
fuchigamiyuki <c1112993@planet.kanazawa-it.ac.jp>
2023-03-02 15:09:10 +0900

    first commit

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -4,6 +4,7 @@
 #include "mcl_2d/mcl_2d_node.hpp"
 #include "controller_interface/controller_interface_node.hpp"
 #include "spline_pid/spline_pid_node.hpp"
+#include "injection_param_calculator/injection_param_calculator.hpp"
 #include <iostream>
 
 int main(int argc, char * argv[]){
@@ -16,13 +17,15 @@ int main(int argc, char * argv[]){
 
     // auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     //auto mcl_2d_node = std::make_shared<mcl_2d::Mcl2D>(nodes_option);
-    auto controller_node = std::make_shared<controller_interface::SmartphoneGamepad>(nodes_option);
-    auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
+    // auto controller_node = std::make_shared<controller_interface::SmartphoneGamepad>(nodes_option);
+    // auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
+    auto injection_param_calclator_node = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option);
 
     // exec.add_node(socketcan_node);
     //exec.add_node(mcl_2d_node);
-    exec.add_node(controller_node);
-    exec.add_node(spline_pid_node);
+    // exec.add_node(controller_node);
+    // exec.add_node(spline_pid_node);
+    exec.add_node(injection_param_calclator_node);
 
     exec.spin();
     rclcpp::shutdown();

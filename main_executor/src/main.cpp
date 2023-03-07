90ecd7f06912578fbc7c8f0f9b1df68a519adc27
RyoFunai <c1013395@planet.kanazawa-it.ac.jp>
2023-03-07 19:29:21 +0900

    RANSAC_localizationノードの追加

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -4,6 +4,7 @@
 #include "mcl_2d/mcl_2d_node.hpp"
 #include "controller_interface/controller_interface_node.hpp"
 #include "spline_pid/spline_pid_node.hpp"
+#include "RANSAC_localization/RANSAC_localization.hpp"
 #include <iostream>
 
 int main(int argc, char * argv[]){
@@ -14,15 +15,15 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
+    auto RANSAC_localization = std::make_shared<self_localization::RANSACLocalization>(nodes_option);
     // auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
-    auto mcl_2d_node = std::make_shared<mcl_2d::Mcl2D>(nodes_option);
-    auto controller_node = std::make_shared<controller_interface::ControllerInterface>(nodes_option);
-    auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
+    // auto controller_node = std::make_shared<controller_interface::ControllerInterface>(nodes_option);
+    // auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
 
     // exec.add_node(socketcan_node);
-    exec.add_node(mcl_2d_node);
-    exec.add_node(controller_node);
-    exec.add_node(spline_pid_node);
+    exec.add_node(RANSAC_localization);
+    // exec.add_node(controller_node);
+    // exec.add_node(spline_pid_node);
 
     exec.spin();
     rclcpp::shutdown();

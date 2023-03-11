0e77bdb0326c5a175073da214c536412e372806d
ryosei1250 <c1103441@planet.kanazawa-it.ac.jp>
2023-03-11 19:48:34 +0900

    手動の上物に関するパラメータを追加。udp_portを削除

--- main_executor/launch/tcp_endpoint.launch.py
+++ main_executor/launch/tcp_endpoint.launch.py
@@ -8,8 +8,18 @@ def generate_launch_description():
             Node(
                 package="ros_tcp_endpoint",
                 executable="default_server_endpoint",
+                name='ros_tcp_endpoint_1',
+                namespace='ros_tcp_endpoint_1',
                 emulate_tty=True,
                 parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 10000}],
+            ),
+            Node(
+                package="ros_tcp_endpoint",
+                executable="default_server_endpoint",
+                name='ros_tcp_endpoint_2',
+                namespace='ros_tcp_endpoint_2',
+                emulate_tty=True,
+                parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 11000}],
             )
         ]
     )
--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -14,15 +14,17 @@ int main(int argc, char * argv[]){
     nodes_option.allow_undeclared_parameters(true);
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
-    // auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
+    //auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
     //auto mcl_2d_node = std::make_shared<mcl_2d::Mcl2D>(nodes_option);
-    auto controller_node = std::make_shared<controller_interface::SmartphoneGamepad>(nodes_option);
+    auto controller_node = std::make_shared<controller_interface::SmartphoneGamepad>(nodes_option, 1, 50000);
+    auto controller_node2 = std::make_shared<controller_interface::SmartphoneGamepad>(nodes_option, 2, 51000);
     //auto controller_node = std::make_shared<controller_interface::DualSense>(nodes_option);
     auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
 
-    // exec.add_node(socketcan_node);
+    //exec.add_node(socketcan_node);
     //exec.add_node(mcl_2d_node);
     exec.add_node(controller_node);
+    exec.add_node(controller_node2);
     exec.add_node(spline_pid_node);
 
     exec.spin();

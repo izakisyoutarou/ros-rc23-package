4fbfcbadc9ac7965352e0137282e1c5aaa2040be
ryosei1250 <c1103441@planet.kanazawa-it.ac.jp>
2023-04-04 12:47:42 +0900

    tcpのlaunchを変更

--- main_executor/launch/tcp_endpoint.launch.py
+++ main_executor/launch/tcp_endpoint.launch.py
@@ -8,26 +8,16 @@ def generate_launch_description():
             Node(
                 package="ros_tcp_endpoint",
                 executable="default_server_endpoint",
-                name='ros_tcp_endpoint_1',
-                namespace='ros_tcp_endpoint_1',
                 emulate_tty=True,
                 parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 10000}],
+                #parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 11000}],
             ),
             Node(
                 package="ros_tcp_endpoint",
                 executable="default_server_endpoint",
-                name='ros_tcp_endpoint_2',
-                namespace='ros_tcp_endpoint_2',
                 emulate_tty=True,
+                #parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 10000}],
                 parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 11000}],
-            ),
-            Node(
-                package="ros_tcp_endpoint",
-                executable="default_server_endpoint",
-                name='ros_tcp_endpoint_3',
-                namespace='ros_tcp_endpoint_3',
-                emulate_tty=True,
-                parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 12000}],
             )
         ]
     )

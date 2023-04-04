594414eaeb0364b8037a609600f2cd85bbf8f346
fuchigamiyuki <c1112993@planet.kanazawa-it.ac.jp>
2023-04-04 12:26:04 +0900

    injection_param_calculatorの移動

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -22,8 +22,9 @@ int main(int argc, char * argv[]){
     // auto spline_pid_node = std::make_shared<spline_pid::SplinePid>(nodes_option);
     auto injection_param_calclator_node0 = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option, 0);
     auto injection_param_calclator_node1 = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option, 1);
-    //auto injection_param_calclator_node0 = std::make_shared<injection_param_calculator::InjectionParamCalculator>("injection_param_calculator_node0",nodes_option, 0);
-    //auto injection_param_calclator_node1 = std::make_shared<injection_param_calculator::InjectionParamCalculator>("injection_param_calculator_node1",nodes_option, 1);
+    // auto injection_param_calclator_node0 = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option,"er", 0);
+    // auto injection_param_calclator_node1 = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option,"er", 1);
+    // auto injection_param_calclator_node2 = std::make_shared<injection_param_calculator::InjectionParamCalculator>(nodes_option,"rr", 2);
 
     // exec.add_node(socketcan_node);
     //exec.add_node(mcl_2d_node);
@@ -31,6 +32,7 @@ int main(int argc, char * argv[]){
     // exec.add_node(spline_pid_node);
     exec.add_node(injection_param_calclator_node0);
     exec.add_node(injection_param_calclator_node1);
+    // exec.add_node(injection_param_calclator_node2);
 
     exec.spin();
     rclcpp::shutdown();
